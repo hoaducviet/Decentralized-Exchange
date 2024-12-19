1d13d239489d344068ae025528b8f894d4f22d89
hoaducviet <viethoaduc.21@gmail.com>
2024-12-19 08:07:17 +0700

    Add new file for fix case

---
+++ frontend/src/components/chart/TvlChart.tsx
@@ -0,0 +1,102 @@
+"use client"
+
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { ChartConfig, ChartContainer, ChartTooltip } from "@/components/ui/chart"
+import { formatPrice } from "@/utils/formatPrice"
+import { TVL } from '@/lib/type'
+export const description = "A linear area chart"
+
+interface Props {
+    tvls: TVL[];
+}
+
+const chartConfig = {
+    tvl: {
+        label: "TVL",
+        color: "hsl(var(--chart-1))",
+    },
+} satisfies ChartConfig
+
+export default function TVLChart({ tvls }: Props) {
+    const chartData = tvls?.map(({ date, tvl }) => ({ date, tvl: parseFloat(tvl || "0") }))
+
+    return (
+        <Card className="border-none outline-none shadow-none">
+            <CardHeader className="px-0 space-y-3">
+                <CardTitle>TVL</CardTitle>
+                <div className="text-5xl font-medium">${formatPrice(chartData[chartData.length - 1]?.tvl)}</div>
+                <CardDescription>
+                    Past month
+                </CardDescription>
+            </CardHeader>
+            <CardContent className="px-0">
+                <ChartContainer config={chartConfig}>
+                    <AreaChart
+                        accessibilityLayer
+                        data={chartData}
+                        margin={{
+                            left: 36,
+                            right: 48,
+                        }}
+                    >
+                        <defs>
+                            <linearGradient id="areaGradient" x1="0" y1="1" x2="0" y2="0">
+                                <stop offset="0%" className="text-blue-200" stopColor="currentColor" /> {/* Màu bắt đầu */}
+                                <stop offset="50%" className="text-blue-500" stopColor="currentColor" /> {/* Màu giữa */}
+                                <stop offset="100%" className="text-blue-800" stopColor="currentColor" /> {/* Màu kết thúc */}
+                            </linearGradient>
+                        </defs>
+                        <CartesianGrid vertical={false} />
+                        <XAxis
+                            dataKey="date"
+                            tickLine={false}
+                            axisLine={false}
+                            tickMargin={8}
+                            minTickGap={0}
+                            tickFormatter={(value) => {
+                                const date = new Date(value)
+                                return date.toLocaleDateString("en-US", {
+                                    month: "short",
+                                    day: "numeric",
+                                })
+                            }}
+                        />
+                        <ChartTooltip
+                            cursor={false}
+                            defaultIndex={1}
+                            content={({ payload, label }) => {
+                                if (!payload || payload.length === 0) return null;
+                                const tvl = payload.find((item) => item.dataKey === "tvl")?.value as number;
+
+                                return (
+                                    <div className="bg-white/30 dark:bg-transparent dark:border-white dark:border-[1px] dark:border-opacity-20 shadow-xl p-4 min-w-[6vw] space-y-1 rounded-2xl">
+                                        <p className="font-semibold">{`${new Date(label).toLocaleDateString("en-US", {
+                                            month: "short",
+                                            day: "numeric",
+                                            year: "numeric",
+                                        })}`}
+                                        </p>
+                                        <div className="flex flex-row justify-between">
+                                            <div className="opacity-70">TVL</div>
+                                            <div className="font-semibold">{`$${formatPrice(tvl)}`}</div>
+                                        </div>
+                                    </div>
+                                );
+                            }}
+                        />
+                        <Area
+                            dataKey="tvl"
+                            type="linear"
+                            fill="url(#areaGradient)"
+                            fillOpacity={0.4}
+                            stroke="url(#areaGradient)"
+                            stackId="a"
+                            dot={false}
+                        />
+                    </AreaChart>
+                </ChartContainer>
+            </CardContent>
+        </Card>
+    )
+}
---
+++ frontend/src/components/explore/PoolTransactions.tsx
@@ -0,0 +1,67 @@
+'use client'
+import { PoolTransactionsType, Pool, TokenActiveTransaction, LiquidityActiveTransaction } from "@/lib/type";
+import { calculateElapsedTime } from "@/utils/calculateElapsedTime";
+import { formatPrice } from "@/utils/formatPrice";
+import { Card } from "@/components/ui/card"
+
+
+const options = ["Time", "Type", "", "", "LPT", "Price", "Wallet"]
+interface Props {
+    transactions: PoolTransactionsType[];
+    pool: Pool | undefined;
+}
+export default function PoolTransactions({ transactions, pool }: Props) {
+
+    return (
+        <Card className="flex flex-col w-full rounded-2xl border-[1px] shadow-md">
+            <div className="bg-secondary/80 hover:bg-secondary flex flex-row justify-between items-center text-md font-semibold rounded-t-2xl h-[3vw] px-4">
+                <div className="w-[15%] flex flex-row justify-start">{options[0]}</div>
+                <div className="w-[15%] flex flex-row justify-start">{options[1]}</div>
+                <div className="w-[10%] flex flex-row justify-end">{pool?.token1?.symbol}</div>
+                <div className="w-[15%] flex flex-row justify-end">{pool?.token2?.symbol}</div>
+                <div className="w-[15%] flex flex-row justify-end">{options[4]}</div>
+                <div className="w-[15%] flex flex-row justify-end">{options[5]}</div>
+                <div className="w-[15%] flex flex-row justify-end">{options[6]}</div>
+            </div>
+            <div className="flex flex-col w-full max-h-[55vw] overflow-x-auto">
+                {transactions.map((item, index) => {
+                    let transaction: PoolTransactionsType = item
+                    if (item.type.includes('Swap')) {
+                        transaction = item as TokenActiveTransaction
+                        return (
+                            <div key={index}>
+                                <div className={`flex flex-row cursor-pointer hover:bg-secondary/80 dark:hover:bg-white/5 text-md font-medium items-center h-[3.5vw] w-full px-4 border-t-[0.2px] border-gray-300 border-opacity-20 ${index === transactions.length - 1 ? 'rounded-b-2xl' : ''}`} >
+                                    <div className="w-[15%] flex flex-row justify-start font-medium">{calculateElapsedTime(transaction.createdAt)}</div>
+                                    <div className="w-[15%] flex flex-row justify-start">
+                                        <div>{transaction.type}</div>
+                                    </div>
+                                    <div className="w-[10%] flex flex-row justify-end">{transaction.from_token_id?.symbol === pool?.token1_id?.symbol ? transaction.amount_in?.slice(0, transaction.amount_in?.indexOf(".") + 7) : transaction.amount_out?.slice(0, transaction.amount_out?.indexOf(".") + 7)}</div>
+                                    <div className="w-[15%] flex flex-row justify-end">{transaction.to_token_id?.symbol === pool?.token1_id?.symbol ? transaction.amount_out?.slice(0, transaction.amount_out?.indexOf(".") + 7) : transaction.amount_in?.slice(0, transaction.amount_in?.indexOf(".") + 7)}</div>
+                                    <div className="w-[15%] flex flex-row justify-end">{""}</div>
+                                    <div className="w-[15%] flex flex-row justify-end">${formatPrice(parseFloat(transaction.price ?? ""))}</div>
+                                    <div className="w-[15%] flex flex-row justify-end">{transaction.from_wallet?.slice(0, 6) + "..." + transaction.from_wallet?.slice(38)}</div>
+                                </div>
+                            </div>
+                        )
+                    }
+                    transaction = item as LiquidityActiveTransaction
+                    return (
+                        <div key={index}>
+                            <div className="cursor-pointer flex flex-row justify-between items-center text-md hover:bg-secondary/80 h-[3.5vw] px-3">
+                                <div className="w-[15%] flex flex-row justify-start font-medium">{calculateElapsedTime(transaction.createdAt)}</div>
+                                <div className="w-[15%] flex flex-row justify-start">
+                                    <div>{transaction.type}</div>
+                                </div>
+                                <div className="w-[10%] flex flex-row justify-end">{transaction.token1_id?.symbol === pool?.token1_id?.symbol ? transaction.amount_token1?.slice(0, transaction.amount_token1?.indexOf(".") + 7) : transaction.amount_token2?.slice(0, transaction.amount_token2?.indexOf(".") + 7)}</div>
+                                <div className="w-[15%] flex flex-row justify-end">{transaction.token1_id?.symbol === pool?.token1_id?.symbol ? transaction.amount_token2?.slice(0, transaction.amount_token2?.indexOf(".") + 7) : transaction.amount_token1?.slice(0, transaction.amount_token1?.indexOf(".") + 7)}</div>
+                                <div className="w-[15%] flex flex-row justify-end">{formatPrice(parseFloat(transaction.amount_lpt ?? ""))}</div>
+                                <div className="w-[15%] flex flex-row justify-end">${formatPrice(parseFloat(transaction.price ?? ""))}</div>
+                                <div className="w-[15%] flex flex-row justify-end">{transaction.wallet?.slice(0, 6) + "..." + transaction.wallet?.slice(38)}</div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </Card>
+    )
+}
